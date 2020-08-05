91da73389c735c649dd0673ea202413fcda7c6d6
haxul <s.s.starodubov@yandex.ru>
2020-08-05 11:28:00 +0400

    recap scala

---
+++ README.md
@@ -0,0 +1,4 @@
+#### Run project
+
+/spark-cluster: docker-compose up --scale spark-worker=3
+/: docker-compose up
--- src/main/scala/part1recap/ScalaRecap.scala
+++ src/main/scala/part1recap/ScalaRecap.scala
@@ -5,109 +5,61 @@ import scala.util.{Failure, Success}
 
 object ScalaRecap extends App {
 
-  // values and variables
-  val aBoolean: Boolean = false
-
-  // expressions
-  val anIfExpression = if(2 > 3) "bigger" else "smaller"
-
-  // instructions vs expressions
-  val theUnit = println("Hello, Scala") // Unit = "no meaningful value" = void in other languages
-
-  // functions
-  def myFunction(x: Int) = 42
-
-  // OOP
   class Animal
-  class Cat extends Animal
+
+  class Dog extends Animal
+
   trait Carnivore {
     def eat(animal: Animal): Unit
   }
 
   class Crocodile extends Animal with Carnivore {
-    override def eat(animal: Animal): Unit = println("Crunch!")
+    override def eat(animal: Animal): Unit = println("crunch")
   }
 
-  // singleton pattern
-  object MySingleton
+  object Carnivore {
+    def growl(): Unit = println("growwwwwl")
+  }
 
-  // companions
-  object Carnivore
+  val increment: Int => Int = (x: Int) => x + 1
 
-  // generics
-  trait MyList[A]
-
-  // method notation
-  val x = 1 + 2
-  val y = 1.+(2)
-
-  // Functional Programming
-  val incrementer: Int => Int = x => x + 1
-  val incremented = incrementer(42)
-
-  // map, flatMap, filter
-  val processedList = List(1,2,3).map(incrementer)
-
-  // Pattern Matching
   val unknown: Any = 45
   val ordinal = unknown match {
-    case 1 => "first"
-    case 2 => "second"
-    case _ => "unknown"
+    case 45 => "HH"
+    case _ => "really unknown"
   }
 
-  // try-catch
-  try {
-    throw new NullPointerException
-  } catch {
-    case _: NullPointerException => "some returned value"
-    case _: Throwable => "something else"
-  }
-
-  // Future
   import scala.concurrent.ExecutionContext.Implicits.global
+
   val aFuture = Future {
-    // some expensive computation, runs on another thread
+    Thread.sleep(1000)
     42
   }
 
   aFuture.onComplete {
-    case Success(meaningOfLife) => println(s"I've found $meaningOfLife")
-    case Failure(ex) => println(s"I have failed: $ex")
+    case Success(age) => println(age)
+    case Failure(exception) => println(s"sorry, $exception is up")
   }
 
-  // Partial functions
   val aPartialFunction: PartialFunction[Int, Int] = {
     case 1 => 43
-    case 8 => 56
-    case _ => 999
+    case _ => 1000
   }
 
-  // Implicits
+  def autoImplicit(implicit x: Int): Int = x + 43
 
-  // auto-injection by the compiler
-  def methodWithImplicitArgument(implicit x: Int) = x + 43
-  implicit val implicitInt = 67
-  val implicitCall = methodWithImplicitArgument
+  implicit val number: Int = 1
 
-  // implicit conversions - implicit defs
   case class Person(name: String) {
-    def greet = println(s"Hi, my name is $name")
+    def greet: String = s"hello my name is $name"
   }
 
-  implicit def fromStringToPerson(name: String) = Person(name)
-  "Bob".greet // fromStringToPerson("Bob").greet
+  implicit def fromStringToPerson(name: String): Person = Person(name)
+//  println("john".greet)
 
-  // implicit conversion - implicit classes
-  implicit class Dog(name: String) {
-    def bark = println("Bark!")
+  implicit class Cat(name:String) {
+    def mow:Unit = println("mow")
   }
-  "Lassie".bark
 
-  /*
-    - local scope
-    - imported scope
-    - companion objects of the types involved in the method call
-   */
-
+//  "cat".mow
 }
