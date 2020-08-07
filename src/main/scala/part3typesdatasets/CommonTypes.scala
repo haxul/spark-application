251e7e322a66f354542d2f695f405f8979c23fe6
haxul <s.s.starodubov@yandex.ru>
2020-08-07 12:31:37 +0400

    lesson 15

--- src/main/scala/part3typesdatasets/CommonTypes.scala
+++ src/main/scala/part3typesdatasets/CommonTypes.scala
@@ -1,6 +1,6 @@
 package part3typesdatasets
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Encoders, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.FloatType
 
@@ -11,82 +11,56 @@ object CommonTypes extends App {
     .config("spark.master", "local")
     .getOrCreate()
 
+  val path = "src/main/resources/data"
   val moviesDF = spark.read
     .option("inferSchema", "true")
-    .json("src/main/resources/data/movies.json")
+    .json(s"$path/movies.json")
 
-  // adding a plain value to a DF
   moviesDF.select(col("Title"), lit(47).as("plain_value"))
-
-  // Booleans
-  val dramaFilter = col("Major_Genre") equalTo "Drama"
+  val dramFilter = col("Major_Genre") === "Drama"
   val goodRatingFilter = col("IMDB_Rating") > 7.0
-  val preferredFilter = dramaFilter and goodRatingFilter
 
-  moviesDF.select("Title").where(dramaFilter)
-  // + multiple ways of filtering
+  moviesDF.select("Title").where(dramFilter and goodRatingFilter)
+  val moviesWithGoodFlagDF  = moviesDF.select(col("Title"), (dramFilter and goodRatingFilter).as("is_good_film"))
+  moviesWithGoodFlagDF.where("is_good_film") // where col("is_good_film") === true
+  moviesWithGoodFlagDF.where(not(col("is_good_film"))) // where col("is_good_film") =!= true
 
-  val moviesWithGoodnessFlagsDF = moviesDF.select(col("Title"), preferredFilter.as("good_movie"))
-  // filter on a boolean column
-  moviesWithGoodnessFlagsDF.where("good_movie") // where(col("good_movie") === "true")
+  moviesDF.select(col("Title"), (col("Rotten_Tomatoes_Rating") / 10 + col("IMDB_Rating") / 2))
 
-  // negations
-  moviesWithGoodnessFlagsDF.where(not(col("good_movie")))
-
-  // Numbers
-  // math operators
-  val moviesAvgRatingsDF = moviesDF.select(col("Title"), (col("Rotten_Tomatoes_Rating") / 10 + col("IMDB_Rating")) / 2)
-
-  // correlation = number between -1 and 1
-  println(moviesDF.stat.corr("Rotten_Tomatoes_Rating", "IMDB_Rating") /* corr is an ACTION */)
-
-  // Strings
+  println(moviesDF.stat.corr("Rotten_Tomatoes_Rating", "IMDB_Rating"))
 
   val carsDF = spark.read
     .option("inferSchema", "true")
-    .json("src/main/resources/data/cars.json")
+    .json(s"$path/cars.json")
 
-  // capitalization: initcap, lower, upper
   carsDF.select(initcap(col("Name")))
-
-  // contains
   carsDF.select("*").where(col("Name").contains("volkswagen"))
-
-  // regex
-  val regexString = "volkswagen|vw"
-  val vwDF = carsDF.select(
+  val regex = "volkswagen|vw"
+  val wvCarDF = carsDF.select(
     col("Name"),
-    regexp_extract(col("Name"), regexString, 0).as("regex_extract")
+    regexp_extract(col("Name"), regex, 0).as("regex_extract")
   ).where(col("regex_extract") =!= "").drop("regex_extract")
 
-  vwDF.select(
+
+  wvCarDF.select(
     col("Name"),
-    regexp_replace(col("Name"), regexString, "People's Car").as("regex_replace")
+    regexp_replace(col("Name"), regex, "People's car").as("replace")
   )
 
-  /**
-    * Exercise
-    *
-    * Filter the cars DF by a list of car names obtained by an API call
-    * Versions:
-    *   - contains
-    *   - regexes
-    */
 
   def getCarNames: List[String] = List("Volkswagen", "Mercedes-Benz", "Ford")
 
-  // version 1 - regex
-  val complexRegex = getCarNames.map(_.toLowerCase()).mkString("|") // volskwagen|mercedes-benz|ford
+  val reg = getCarNames.map(_.toLowerCase).mkString("|")
+
   carsDF.select(
     col("Name"),
-    regexp_extract(col("Name"), complexRegex, 0).as("regex_extract")
-  ).where(col("regex_extract") =!= "")
-    .drop("regex_extract")
+    regexp_extract(col("Name"), reg, 0).as("match")
+  ).where(col("match") =!= "").drop(col("match"))
 
-  // version 2 - contains
-  val carNameFilters = getCarNames.map(_.toLowerCase()).map(name => col("Name").contains(name))
-  val bigFilter = carNameFilters.fold(lit(false))((combinedFilter, newCarNameFilter) => combinedFilter or newCarNameFilter)
-  carsDF.filter(bigFilter).show
+  val carNamesFilterList = getCarNames.map(_.toLowerCase).map(name => col("Name").contains(name))
+  val bigFilter = carNamesFilterList.fold(lit(false)) ((f1, f2) => f1 or f2)
 
-
+  carsDF.select(
+    col("Name")
+  ).where(bigFilter).show
 }
