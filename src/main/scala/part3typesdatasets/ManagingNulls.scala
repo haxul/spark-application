7a17a7666a8088bc0a555378c680c4949ba1aea4
haxul <s.s.starodubov@yandex.ru>
2020-08-07 15:06:15 +0400

    lesson 17

--- src/main/scala/part3typesdatasets/ManagingNulls.scala
+++ src/main/scala/part3typesdatasets/ManagingNulls.scala
@@ -14,40 +14,29 @@ object ManagingNulls extends App {
     .option("inferSchema", "true")
     .json("src/main/resources/data/movies.json")
 
-
-  // select the first non-null value
   moviesDF.select(
     col("Title"),
     col("Rotten_Tomatoes_Rating"),
     col("IMDB_Rating"),
-    coalesce(col("Rotten_Tomatoes_Rating"), col("IMDB_Rating") * 10)
+    coalesce(col("Rotten_Tomatoes_Rating"), col("IMDB_Rating"))
   )
 
-  // checking for nulls
-  moviesDF.select("*").where(col("Rotten_Tomatoes_Rating").isNull)
+  moviesDF.select("*").where(col("Rotten_Tomatoes_Rating").isNotNull)
 
-  // nulls when ordering
   moviesDF.orderBy(col("IMDB_Rating").desc_nulls_last)
-
-  // removing nulls
-  moviesDF.select("Title", "IMDB_Rating").na.drop() // remove rows containing nulls
-
-  // replace nulls
+  moviesDF.select("Title", "IMDB_Rating").na.drop()
   moviesDF.na.fill(0, List("IMDB_Rating", "Rotten_Tomatoes_Rating"))
   moviesDF.na.fill(Map(
     "IMDB_Rating" -> 0,
-    "Rotten_Tomatoes_Rating" -> 10,
-    "Director" -> "Unknown"
+    "Rotten_Tomatoes_Rating" -> 10
   ))
 
-  // complex operations
   moviesDF.selectExpr(
     "Title",
-    "IMDB_Rating",
     "Rotten_Tomatoes_Rating",
-    "ifnull(Rotten_Tomatoes_Rating, IMDB_Rating * 10) as ifnull", // same as coalesce
-    "nvl(Rotten_Tomatoes_Rating, IMDB_Rating * 10) as nvl", // same
-    "nullif(Rotten_Tomatoes_Rating, IMDB_Rating * 10) as nullif", // returns null if the two values are EQUAL, else first value
-    "nvl2(Rotten_Tomatoes_Rating, IMDB_Rating * 10, 0.0) as nvl2" // if (first != null) second else third
-  ).show()
+    "IMDB_Rating",
+    "ifnull(IMDB_Rating, 10)",
+    "nvl(IMDB_Rating, 10)",
+    "nullif(IMDB_Rating, 10)",
+  )
 }
