6b0a11e52a15e4613e2e64cf7f130969cedfc609
haxul <s.s.starodubov@yandex.ru>
2020-08-07 10:35:06 +0400

    lesson 13

--- src/main/scala/part2dataframes/Joins.scala
+++ src/main/scala/part2dataframes/Joins.scala
@@ -1,7 +1,7 @@
 package part2dataframes
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{array_contains, col, expr, max}
+import org.apache.spark.sql.functions.{array_contains, col}
 
 object Joins extends App {
 
@@ -35,7 +35,62 @@ object Joins extends App {
   val modBandsIdDF = bandsDF.withColumnRenamed("id", "bandId")
   guitaristsDF.join(modBandsIdDF, guitaristsDF.col("band") === modBandsIdDF.col("bandId"), "left")
 
-  guitaristsDF.join(guitarsDF.withColumnRenamed("id", "guitarId"), array_contains(col("guitars"), col("guitarId")), "left").show()
+  guitaristsDF.join(guitarsDF.withColumnRenamed("id", "guitarId"), array_contains(col("guitars"), col("guitarId")), "left")
+
+
+
+  val employeesDF = spark.read
+    .format("jdbc")
+    .option("driver", "org.postgresql.Driver")
+    .option("url", "jdbc:postgresql://localhost:5432/rtjvm")
+    .option("user", "docker")
+    .option("password", "docker")
+    .option("dbtable", "public.employees")
+    .load()
+
+  val salariesDF = spark.read
+    .format("jdbc")
+    .option("driver", "org.postgresql.Driver")
+    .option("url", "jdbc:postgresql://localhost:5432/rtjvm")
+    .option("user", "docker")
+    .option("password", "docker")
+    .option("dbtable", "public.salaries")
+    .load()
+
+  val managerDF = spark.read
+    .format("jdbc")
+    .option("driver", "org.postgresql.Driver")
+    .option("url", "jdbc:postgresql://localhost:5432/rtjvm")
+    .option("user", "docker")
+    .option("password", "docker")
+    .option("dbtable", "public.dept_manager")
+    .load()
+
+  val titlesDF = spark.read
+    .format("jdbc")
+    .option("driver", "org.postgresql.Driver")
+    .option("url", "jdbc:postgresql://localhost:5432/rtjvm")
+    .option("user", "docker")
+    .option("password", "docker")
+    .option("dbtable", "public.titles")
+    .load()
+  // 1
+  employeesDF.join(salariesDF, employeesDF.col("emp_no") === salariesDF.col("emp_no"), "left")
+    .select(employeesDF.col("emp_no"), salariesDF.col("salary")).groupBy("emp_no").max("salary")
+  // 2
+
+  employeesDF.join(managerDF, employeesDF.col("emp_no") === managerDF.col("emp_no"), "left_anti")
+
+  // 3
+
+  employeesDF.join(salariesDF, employeesDF.col("emp_no") === salariesDF.col("emp_no"), "left")
+    .select(employeesDF.col("emp_no"), salariesDF.col("salary"))
+    .groupBy("emp_no")
+    .max("salary")
+    .join(titlesDF, employeesDF.col("emp_no") === titlesDF.col("emp_no"), "left")
+    .select(employeesDF.col("emp_no"), col("max(salary)"), col("title")).
+    orderBy(col("max(salary)").desc)
+
 
 
 }
